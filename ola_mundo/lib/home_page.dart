55a10fb78bb255f8ff59add7162eab1ec4d47672
Peter <pivow@hotmail.com>
2023-01-26 20:05:14 -0300

    Aula 13 componentização

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -16,16 +16,10 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: [CustomSwitch()],
       ),
       body: Center(
-        child: Switch(
-          value: AppController.instance.isDarkTheme,
-          onChanged: (value) {
-            setState(() {
-              AppController.instance.changeTheme();
-            });
-          },
-        ),
+        child: CustomSwitch(),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -35,6 +29,20 @@ class HomePageState extends State<HomePage> {
           });
         },
       ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        {
+          AppController.instance.changeTheme();
+        }
+      },
     );
   }
 }
