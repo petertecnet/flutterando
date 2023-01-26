5c8c1090212df23a3fdc213587a10b5d464951ec
Peter <pivow@hotmail.com>
2023-01-26 16:11:12 -0300

    Aula 9 Boas Práticas

---
+++ ola_mundo/lib/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ola_mundo/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
