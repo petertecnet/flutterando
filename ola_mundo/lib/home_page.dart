269ce833e38fcc3087d5a664d80a19f008cf18ce
Peter <pivow@hotmail.com>
2023-01-26 16:42:19 -0300

    Aula 11 Single render e Multi render

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -16,15 +16,17 @@ class HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Center(
-          child: GestureDetector(
-        child: Text('Contador: $counter', style: TextStyle(fontSize: 20)),
-        onTap: () {
-          setState(() {
-            counter++;
-          });
-        },
-      )),
+      body: Container(
+          height: 200,
+          width: 200,
+          color: Colors.black,
+          child: Center(
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+            ),
+          )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
