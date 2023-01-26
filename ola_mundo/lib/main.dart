a560cb81072375e9229ee385a8572083c2e4c8df
Peter <pivow@hotmail.com>
2023-01-26 13:03:29 -0300

    Aula 6 stateless

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-      child: Center(
-    child: Text(
-      'Fluterando',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.black, fontSize: 50.0),
-    ),
-  )));
+  runApp(AppWidget(
+    title: 'Fluterando',
+  ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text(
+        title,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.black, fontSize: 50.0),
+      ),
+    ));
+  }
 }
