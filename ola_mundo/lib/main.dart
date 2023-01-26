6341978a6ec0ee61d8ea973b414aac822ac1a525
Peter <pivow@hotmail.com>
2023-01-26 12:52:54 -0300

    Aula 5 Widget

---
+++ ola_mundo/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Container(
+      child: Center(
+    child: Text(
+      'Fluterando',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.black, fontSize: 50.0),
+    ),
+  )));
+}
